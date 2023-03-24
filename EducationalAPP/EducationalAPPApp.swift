d112045b8571dce90782a642b99a6bab43d18a83
Shota <sh.klibadze@gmail.com>
2023-03-24 17:59:47 +0400

    Initial Commit

---
+++ EducationalAPP/EducationalAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationalAPPApp.swift
+//  EducationalAPP
+//
+//  Created by Shotiko Klibadze on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct EducationalAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
